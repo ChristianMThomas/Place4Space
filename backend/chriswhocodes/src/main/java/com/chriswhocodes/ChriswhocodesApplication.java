76e2d714805fa0d4941fd3bf10ad27afa5e7b8ff
Kvng Chris <christhomas0634@gmail.com>
2025-12-15 23:13:43 -0500

    Initalized Backend Files using Spring Initilzr

---
+++ backend/chriswhocodes/src/main/java/com/chriswhocodes/ChriswhocodesApplication.java
@@ -0,0 +1,13 @@
+package com.chriswhocodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChriswhocodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChriswhocodesApplication.class, args);
+	}
+
+}
---
+++ backend/chriswhocodes/src/test/java/com/chriswhocodes/ChriswhocodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chriswhocodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChriswhocodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
